a59fd2b7abdd7b741821c2d8496c547fa489b907
Florian Jung - Fixe <florianjung885@gmail.com>
2019-10-17 16:14:55 +0200

    ajout nouvel utilisateur

--- src/Composants/Rendu/Utilisateurs/Gestion.js
+++ src/Composants/Rendu/Utilisateurs/Gestion.js
@@ -1,6 +1,8 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useState, useRef, useReducer } from "react";
 import styled from "styled-components";
 import { withCookies } from "react-cookie";
+import "./Gestion.css";
+import { CSSTransition } from "react-transition-group";
 import {
     Table,
     Card,
@@ -10,7 +12,9 @@ import {
     Popconfirm,
     Tooltip,
     Modal,
-    Input
+    Input,
+    Form,
+    Select
 } from "antd";
 import axios from "axios";
 
@@ -22,7 +26,236 @@ const Conteneur = styled.div`
     overflow: auto;
 `;
 
+const { Item } = Form;
+
+function hasErrors(fieldsError) {
+    return Object.keys(fieldsError).some((field) => fieldsError[field]);
+}
+const formItemLayout = {
+    labelCol: {
+        span: 6,
+        offset: 2
+    },
+    wrapperCol: {
+        span: 15
+    }
+};
+const Formulaire = (props) => {
+    const [etape, setEtape] = useState("init");
+    const valueButton = {
+        init: "Créer l'utilisateur",
+        update: "En cours de création",
+        finOk: "Utilisateur créé",
+        finPasOk: "Problème à la création."
+    };
+    const [attValid, setAttValid] = useState(false);
+    const ax = axios.create({
+        baseURL: "http://phidbac.fr:4000/",
+        headers: { Authorization: props.cookies.get("token") },
+        responseType: "json"
+    });
+    const {
+        getFieldDecorator,
+        getFieldsError,
+        getFieldError,
+        isFieldTouched,
+        validateFields
+    } = props.form;
+
+    const usernameError =
+        isFieldTouched("username") && getFieldError("username");
+    const passwordError =
+        isFieldTouched("password") && getFieldError("password");
+
+    function subForm(e) {
+        e.preventDefault();
+        validateFields((err, values) => {
+            if (!err) {
+                setAttValid(true);
+                setEtape("update");
+                ax.post("/UtilisateurAjout", {
+                    email: values.email,
+                    nom: values.nom,
+                    prenom: values.prenom,
+                    grade: values.grade,
+                    localisation: values.ville,
+                    password: values.password,
+                    actif: true
+                })
+                    .then((rep) => {
+                        setAttValid(false);
+                        setEtape("finOk");
+                        console.log(props);
+                        setTimeout(() => {
+                            props.ouvert(false);
+                        }, 2000);
+                    })
+                    .catch((err) => setEtape("finPasOk"));
+            }
+        });
+    }
+    useEffect(() => {
+        console.log(props);
+    }, []);
+    return (
+        <Form
+            {...formItemLayout}
+            title="Nouvel utilisateur"
+            onSubmit={(e) => subForm(e)}
+        >
+            <Item label="Prénom :" hasFeedback>
+                {getFieldDecorator("prenom", {
+                    rules: [
+                        {
+                            required: true,
+                            message: "Ce champ est requis."
+                        }
+                    ]
+                })(
+                    <Input
+                        prefix={
+                            <Icon
+                                type="user"
+                                style={{ color: "rgba(0,0,0,.25)" }}
+                            />
+                        }
+                        placeholder="Prénom"
+                    />
+                )}
+            </Item>
+            <Item label="Nom :" hasFeedback>
+                {getFieldDecorator("nom", {
+                    rules: [
+                        {
+                            required: true,
+                            message: "Ce champ est requis."
+                        }
+                    ]
+                })(
+                    <Input
+                        prefix={
+                            <Icon
+                                type="user"
+                                style={{ color: "rgba(0,0,0,.25)" }}
+                            />
+                        }
+                        placeholder="Nom"
+                    />
+                )}
+            </Item>
+            <Item label="Ville :" hasFeedback>
+                {getFieldDecorator("ville", {})(
+                    <Input
+                        prefix={
+                            <Icon
+                                type="environment"
+                                style={{ color: "rgba(0,0,0,.25)" }}
+                            />
+                        }
+                        placeholder="Ville"
+                    />
+                )}
+            </Item>
+            <Item label="Grade :" hasFeedback>
+                {getFieldDecorator("grade", {
+                    rules: [
+                        {
+                            required: true,
+                            message: "Ce champ est requis."
+                        }
+                    ]
+                })(
+                    <Select placeholder="Droits de l'utilisateur">
+                        <Select.Option value="Administrateur">
+                            Administrateur
+                        </Select.Option>
+                        <Select.Option value="Visiteur">Visiteur</Select.Option>
+                        <Select.Option value="Eleve">Eleve</Select.Option>
+                    </Select>
+                )}
+            </Item>
+            <Item label="Email :" hasFeedback>
+                {getFieldDecorator("email", {
+                    rules: [
+                        {
+                            required: true,
+                            message: "Ce champ est requis."
+                        },
+                        {
+                            message: "Ce n'est pas un email.",
+                            pattern: new RegExp(
+                                "^[a-zA-Z0-9.!#$%&*+/=?^_]+@[a-zA-Z]+.[a-zA-Z]{2,10}$"
+                            )
+                        }
+                    ]
+                })(
+                    <Input
+                        prefix={
+                            <Icon
+                                type="mail"
+                                style={{ color: "rgba(0,0,0,.25)" }}
+                            />
+                        }
+                        placeholder="Adresse mél"
+                    />
+                )}
+            </Item>
+            <Item
+                label="Mot de passe :"
+                hasFeedback
+                help={passwordError || "6 caractères minimum"}
+            >
+                {getFieldDecorator("password", {
+                    rules: [
+                        {
+                            required: true,
+                            message: "Ce champ est requis."
+                        },
+                        {
+                            pattern: new RegExp("^.{6,}$"),
+                            message: "Moins de 6 caractères !"
+                        }
+                    ]
+                })(
+                    <Input.Password
+                        prefix={
+                            <Icon
+                                type="lock"
+                                style={{ color: "rgba(0,0,0,.25)" }}
+                            />
+                        }
+                        type="password"
+                        placeholder="Mot de passe"
+                    />
+                )}
+            </Item>
+            <Item wrapperCol={24} style={{ marginBottom: "0px" }}>
+                <Button
+                    loading={attValid}
+                    icon={etape === "finOk" ? "check" : null}
+                    type="primary"
+                    htmlType="submit"
+                    disabled={hasErrors(getFieldsError())}
+                    block
+                    style={{
+                        marginTop: "10px",
+                        backgroundColor:
+                            !attValid && etape !== "init"
+                                ? etape === "finOk"
+                                    ? "#52c41a"
+                                    : "lightsalmon"
+                                : null
+                    }}
+                >
+                    {valueButton[etape]}
+                </Button>
+            </Item>
+        </Form>
+    );
+};
+
 const Gestion = (props) => {
+    const [nouvelUtilisateur, setNouvelUtilisateur] = useState(false);
     const [modif, setModif] = useState(false);
     const [pass, setPass] = useState();
     const [idSel, setIdSel] = useState();
@@ -129,7 +362,7 @@ const Gestion = (props) => {
                         title="Souhaitez-vous supprimer cet utilisateur ?"
                         cancelText="Annuler"
                         okText="Supprimer"
-                        placement="topRight"
+                        placement="bottomRight"
                         onConfirm={() => suppressionUtilisateur(id)}
                     >
                         <Tooltip title="Supprimer cet utilisateur">
@@ -168,6 +401,30 @@ const Gestion = (props) => {
                     onChange={(e) => setPass(e.target.value)}
                 ></Input>
             </Modal>
+            <Button
+                icon="plus"
+                style={{ marginBottom: "10px" }}
+                type="primary"
+                onClick={() => setNouvelUtilisateur(!nouvelUtilisateur)}
+            >
+                Ajouter un utilisateur
+            </Button>
+            <CSSTransition
+                unmountOnExit
+                in={nouvelUtilisateur}
+                classNames="utili"
+                timeout={300}
+            >
+                <Card className="test" title="Nouvel utilisateur">
+                    <TestLogin
+                        cookies={props.cookies}
+                        ouvert={(val) => {
+                            setNouvelUtilisateur(val);
+                            setModif(!modif);
+                        }}
+                    />
+                </Card>
+            </CSSTransition>
             <Card>
                 <Table
                     sortable
@@ -180,5 +437,7 @@ const Gestion = (props) => {
         </Conteneur>
     );
 };
+
+const TestLogin = Form.create({ name: "FormTest" })(Formulaire);
 
 export default withCookies(Gestion);
